18923a596f092fe11644d76f29d2331e2f993641
PiplaniRitik <96330982+PiplaniRitik@users.noreply.github.com>
2023-06-10 12:34:10 +0530

    initial setup

---
+++ src/components/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div>
+      App Name: Find_Your_Recipe
+      <br></br>
+      Motive: You can look for your Recipe by Searching
+      <br></br>
+      Developer: Ritik Piplani
+
+    </div>
+  )
+}
